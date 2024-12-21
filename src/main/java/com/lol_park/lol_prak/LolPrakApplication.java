15a9226818e4d92a3266cd5705688988f3e8a01d
leewoojin <zxsa999o@gmail.com>
2024-12-21 14:54:33 +0900

    Initial commit

---
+++ src/main/java/com/lol_park/lol_prak/LolPrakApplication.java
@@ -0,0 +1,13 @@
+package com.lol_park.lol_prak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolPrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolPrakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lol_park/lol_prak/LolPrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lol_park.lol_prak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolPrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
